390cf9375447d433bb023924d60957a348ad3f78
AjayAyrekar <ajayayrekaraj@gmail.com>
2025-09-22 19:24:52 +0530

    Code

--- datatypes_revision/fib.java
+++ datatypes_revision/fib.java
@@ -1,5 +1,18 @@
 package datatypes_revision;
-
+import java.util.*;
 public class fib {
-
-}
+	public static void main(String[]args) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int one=0;
+	int two=1;
+	for(int i=1;i<=n;i++) {
+		System.out.print(one+" ");
+		int sum=one+two;
+		one=two;
+		two=sum;
+	}
+	System.out.println(one);
+	
+	}
+} 
